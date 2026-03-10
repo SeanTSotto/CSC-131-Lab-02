7d81b96241250ea8849b5178459e5ad4eafcd719
lauren <laurenoliaei@csus.edu>
2026-03-10 10:31:06 -0700

    Add new line for Lauren Oliaei in Hello.java

--- Hello.java
+++ Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("304086458 - This line was added by Sean Sotto.");
 		System.out.println("303792840 - This line was added by Edison Duong.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("304946538 - This line was added by Lauren Oliaei.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
@@ -18,5 +18,6 @@ public class Hello {
 }
 
 }
+
 
 
