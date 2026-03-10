5d0dfbf4daa99622df7bdd390fc1ad2d2d971424
SeanTSotto <seansotto@csus.edu>
2026-03-09 18:41:50 -0700

    Update Hello.java
    updated with my sacstate id

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by Sean Sotto.");
+		System.out.println("304086458 - This line was added by Sean Sotto.");
 		System.out.println("303792840 - This line was added by Edison Duong.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
@@ -18,4 +18,5 @@ public class Hello {
 }
 
 }
+
 
